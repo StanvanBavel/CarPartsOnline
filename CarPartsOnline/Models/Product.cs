a00e687911939a3e749580a592567ad62fc749d2
StanvanBavel <73948369+StanvanBavel@users.noreply.github.com>
2022-01-12 16:13:24 +0100

    update database

--- CarPartsOnline/Models/Product.cs
+++ CarPartsOnline/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace CarPartsOnline.Models
         public string productName { get; set; }
         public string productDescription { get; set; }
         public string productImage { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal productPrice { get; set; }
         
 
